4ba10f123ed431f0be90957b0e1a781239fe4ce6
salahali1000 <124151783+salahali1000@users.noreply.github.com>
2023-09-09 21:06:18 +0300

    Add files via upload

---
+++ MCAL/ADC/ADC.h
@@ -0,0 +1,17 @@
+/*
+ * ADC.h
+ *
+ * Created: 4/17/2023 4:15:21 PM
+ *  Author: salah
+ */ 
+
+
+#ifndef ADC_H_
+#define ADC_H_
+void ADC_INIT(void);
+u16 ADC_U16READ(void);
+
+
+
+
+#endif /* ADC_H_ */
